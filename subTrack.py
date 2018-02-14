8f1e13d11291a725a03e2aa4a3274e755223e2dc
nowrd2xpln <allen.mamaril@gmail.com>
2018-02-13 19:41:18 -0800

    Implent html page save

--- subTrack.py
+++ subTrack.py
@@ -10,9 +10,16 @@ def main():
     get_used_subies('capital')
     print('len %d' % len(subie_dealer) )    
 
+#def save_site(site):
+#    open('page.txt', 'wb').write(site.content)
+    
 def get_used_subies(dealer):
     response = simple_get('https://www.capitolsubarusj.com/used-inventory/index.htm?compositeType=&year=&make=Subaru&model=Forester&trim=&bodyStyle=&driveLine=&internetPrice=&saveFacetState=true&lastFacetInteracted=inventory-listing1-facet-anchor-model-1')
      
+    #save_site(response)
+    print(type(response))
+    open('page.txt', 'wb').write(response)
+
     if response is not None:
         html = BeautifulSoup(response, 'html.parser')
         names = list()
