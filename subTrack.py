5cef2f55bb08eac9e50c2245af07c716580bc3a5
nowrd2xpln <allen.mamaril@gmail.com>
2018-02-13 20:46:08 -0800

    Parse out car model line. Split car description into list.

--- subTrack.py
+++ subTrack.py
@@ -10,15 +10,13 @@ def main():
     get_used_subies('capital')
     print('len %d' % len(subie_dealer) )    
 
-#def save_site(site):
-#    open('page.txt', 'wb').write(site.content)
+def save_site(site):
+    open('page.txt', 'wb').write(site)
     
 def get_used_subies(dealer):
     response = simple_get('https://www.capitolsubarusj.com/used-inventory/index.htm?compositeType=&year=&make=Subaru&model=Forester&trim=&bodyStyle=&driveLine=&internetPrice=&saveFacetState=true&lastFacetInteracted=inventory-listing1-facet-anchor-model-1')
      
-    #save_site(response)
-    print(type(response))
-    open('page.txt', 'wb').write(response)
+    save_site(response)
 
     if response is not None:
         html = BeautifulSoup(response, 'html.parser')
@@ -29,9 +27,15 @@ def get_used_subies(dealer):
         for li in html.select('li'):
             for name in li.text.split('\n'):
                 if len(name) > 0:
-                    names.append(name.strip())
-                #print(name)
-                    text_file.write(">%s<\n" % name);
+                    if "Forester" in name:
+                        names.append(name.strip())
+                        print(name)
+                        text_file.write(">%s<\n" % name)
+
+                    if "Engine" in name:
+                        names.append(name.split(","))
+                        text_file.write("***%s\n" % name)
+                        print(name.split(","))
 
         return names
 
