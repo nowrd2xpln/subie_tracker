49d14f11404efdc2f77d4f37420e9b2f50bfcb73
nowrd2xpln <allen.mamaril@gmail.com>
2018-02-16 22:21:39 -0800

    Continue parsing individiual cars

--- subieTrack.py
+++ subieTrack.py
@@ -20,7 +20,7 @@ subie_dealer = {}
 def main():
     print('Get subies!')
     get_used_subies('capital')
-    print('len %d' % len(subie_dealer) )    
+    print('Dealer Count %d' % len(subie_dealer) )    
 
 def save_site(site):
     open('page.txt', 'wb').write(site)
@@ -29,6 +29,8 @@ def get_used_subies(dealer):
     response = simple_get('https://www.capitolsubarusj.com/used-inventory/index.htm?compositeType=&year=&make=Subaru&model=Forester&trim=&bodyStyle=&driveLine=&internetPrice=&saveFacetState=true&lastFacetInteracted=inventory-listing1-facet-anchor-model-1')
      
     save_site(response)
+    
+    carCount = 0
 
     if response is not None:
         html = BeautifulSoup(response, 'html.parser')
@@ -41,23 +43,63 @@ def get_used_subies(dealer):
                 if len(name) > 0:
                     if "Forester" in name:
                         names.append(name.strip())
-                        print(">>>")
-                        print(name)
-                        print("<<<")
                         match = re.search(r"(\d{4})\s+(\w+)\s+(.+)*",name)
                         if match:
-                            print("match:",match.group(0))
-                            print("year:",match.group(1))
-                            print("model:",match.group(2))
-                            print("remaining:",match.group(3))
+                            print("************************************************************")
+                            print("\tyear:",match.group(1))
+                            print("\tmodel:",match.group(2))
+                            print("\tremaining:",match.group(3))
                             
                         text_file.write(">%s<\n" % name)
 
-                    if "Engine" in name:
+                    if "Engine:" in name:
+                        #print("New Car:%s\n" % name)
+                        carCount += 1
+                        
                         names.append(name.split(","))
                         text_file.write("***%s\n" % name)
-                        #print(name.split(","))
+                        attributes = name.split(",")
+                        for item in attributes:
+                            if "Transmission" in item:
+                                #print(item)
+                                match = re.search(r".+:[ ](\w+)", item)
+                                if match:
+                                    m = match.group(1)
+                                    print("\t%s" % m)
+                            if "Exterior Color" in item:
+                                #print(item)
+                                match = re.search(r".+:[ ](\w+)", item)
+                                if match:
+                                    m = match.group(1)
+                                    print("\t%s" % m)
+                            if "Interior Color" in item:
+                                #print(item)
+                                match = re.search(r".+:[ ](\w+)", item)
+                                if match:
+                                    m = match.group(1)
+                                    print("\t%s" % m)
+                            if "Stock #" in item:
+                                #print(item)
+                                match = re.search(r".+:[ ](\w+)", item)
+                                if match:
+                                    m = match.group(1)
+                                    print("\t%s" % m)
+                            if "Model Code" in item:
+                                #print(item)
+                                match = re.search(r".+:[ ](\w+)", item)
+                                if match:
+                                    m = match.group(1)
+                                    print("\t%s" % m)
+                            if "VIN" in item:
+                                #print(item)
+                                match = re.search(r".+:[ ](\w+)", item)
+                                if match:
+                                    m = match.group(1)
+                                    print("\t%s" % m)
+                    continue
+                    print("************************************************************")
 
+        print("Car Count %d" % carCount)
         return names
 
     raise Exception('Error retrieving contents at {}'.format(url))
